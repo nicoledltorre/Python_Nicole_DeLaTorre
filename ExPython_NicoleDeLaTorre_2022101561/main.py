ba8bc8c04778dbfa480e221913150965af61cd28
Nicole DeLaTorre <nicoledltrojas@gmail.com>
2025-05-31 14:23:42 -0300

    agregue mensaje a mi main para que no salga method not allowed :)

--- ExPython_NicoleDeLaTorre_2022101561/main.py
+++ ExPython_NicoleDeLaTorre_2022101561/main.py
@@ -4,5 +4,9 @@ from cliente import cliente
 app = Flask(__name__)
 app.register_blueprint(cliente)
 
+@app.route('/', methods=['GET'])
+def hello():
+    return 'Nicole De La Torre - 2022101561'
+
 if __name__ == '__main__':
     app.run(host='localhost', port=5003, debug=True)
